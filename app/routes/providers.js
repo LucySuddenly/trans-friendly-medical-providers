3f52caf8595e6767bb5de136b382454c5562bfc9
Lucy Suddenly <suddenlylucy@gmail.com>
2019-11-21 13:24:05 -0800

    added basic providers page

--- app/routes/providers.js
+++ app/routes/providers.js
@@ -1,4 +1,8 @@
 import Route from '@ember/routing/route';
-
+import fetch from 'fetch'
 export default Route.extend({
+    model(){
+        return fetch('http://localhost:3000/providers')
+        .then(resp => resp.json())
+    }
 });
